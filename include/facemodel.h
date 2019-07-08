ade8922c0f08518f9f39bff7e4b3632790a4312d
John Flynn <johnflynn90@gmail.com>
2019-07-08 21:09:37 +0200

    Implement loadAverageMesh()

--- include/facemodel.h
+++ include/facemodel.h
@@ -1,11 +1,23 @@
 #ifndef FACEMODEL_H
 #define FACEMODEL_H
 
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+
+static constexpr float SCALE_AVG_MESH = 1 / 1000000.0;
 
 class FaceModel
 {
 public:
     FaceModel();
+
+    int loadAverageMesh();
+
+private:
+    typedef OpenMesh::TriMesh_ArrayKernelT<>  AvgMesh;
+
+private:
+    AvgMesh m_avg_mesh;
+
 };
 
 #endif // FACEMODEL_H
--- src/facemodel.cpp
+++ src/facemodel.cpp
@@ -1,6 +1,43 @@
 #include "facemodel.h"
 
+#include <iostream>
+
+#include <OpenMesh/Core/IO/MeshIO.hh>
+#include <OpenMesh/Tools/Utils/getopt.h>
+
+
+constexpr const char* FILENAME_AVG_MESH = "../models/averageMesh.off";
+
 FaceModel::FaceModel()
 {
+    loadAverageMesh();
+}
 
+int FaceModel::loadAverageMesh() {
+    OpenMesh::IO::Options ropt;
+
+    // Set input options
+    ropt += OpenMesh::IO::Options::VertexColor;
+
+    m_avg_mesh.request_vertex_colors();
+
+    // assure we have vertex normals
+    if (!m_avg_mesh.has_vertex_colors())
+    {
+      std::cerr << "ERROR: Standard vertex property 'Colors' not available for average mesh!\n";
+      return 1;
+    }
+
+    if (!OpenMesh::IO::read_mesh(m_avg_mesh, FILENAME_AVG_MESH, ropt)) {
+        std::cerr << "ERROR: Could not load " << FILENAME_AVG_MESH << "\n";
+        return 1;
+    }
+
+    // The average mesh provided by Justus Thies must be scaled by a factor
+    // AVG_MESH_SCALE
+    for (AvgMesh::VertexIter v_it = m_avg_mesh.vertices_begin();
+         v_it != m_avg_mesh.vertices_end(); ++v_it)
+    {
+      m_avg_mesh.set_point( *v_it, m_avg_mesh.point(*v_it) * SCALE_AVG_MESH );
+    }
 }
